f9e3fd62ef6f0153ccbc3136d08b3222a4f2b870
Anuar Omarov <anuar_omarov@epam.com>
2016-06-06 10:50:07 +0600

    Calculator 1.1 add CalculatorFactory

--- src/main/java/com/epam/calculator/CalculatorFactory.java
+++ src/main/java/com/epam/calculator/CalculatorFactory.java
@@ -1,15 +1,8 @@
 package com.epam.calculator;
 
 public class CalculatorFactory {
-    private static Calculator calculator;
-
-    private CalculatorFactory(int a, int b, int operation) {
-        calculator = new Calculator(a , b ,operation);
-    }
 
     public static Calculator getInstance(int a, int b, int operation) {
-        new CalculatorFactory(a, b, operation);
-        return calculator;
+        return new Calculator(a , b ,operation);
     }
-
 }
